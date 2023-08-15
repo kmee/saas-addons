56cd768cc7d4a4b9700ab14465dc3047e75c9593
Tiago Amaral <tiago.amaral@kmee.com.br>
2023-08-15 16:55:54 -0300

    [ADD] multiple initialization templates in saas_initialization_script_template

--- saas_initialization_script_template/models/saas_template.py
+++ saas_initialization_script_template/models/saas_template.py
@@ -9,7 +9,7 @@ class SAASTemplate(models.Model):
         string='Initialization Template'
     )
 
-    build_initialization_template_id = fields.Many2one(
+    build_initialization_template_ids = fields.Many2many(
         comodel_name='saas.initialization.template',
         string='Build Initialization Template'
     )
@@ -20,8 +20,10 @@ class SAASTemplate(models.Model):
             template_code = self.template_initialization_template_id.template_post_init
             self.template_post_init = template_code
 
-    @api.onchange('build_initialization_template_id')
+    @api.onchange('build_initialization_template_ids')
     def onchange_build_initialization_template(self):
-        if self.build_initialization_template_id:
-            template_code = self.build_initialization_template_id.build_post_init
+        if self.build_initialization_template_ids:
+            template_code = ""
+            for template in self.build_initialization_template_ids:
+                template_code += template.build_post_init + "\n"
             self.build_post_init = template_code
